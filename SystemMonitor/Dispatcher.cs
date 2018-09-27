5a2838aea771afcb0b166bfaa1e45c3b37441751
Luke Frahm <lfrahm@GreatAmerica.com>
2018-09-27 14:50:06 -0500

    Initial commit

---
+++ SystemMonitor/Dispatcher.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SystemMonitor
+{
+    public enum EventAction
+    {
+        Notify,
+        Restart,
+        Shutdown,
+        Crash
+    }
+
+    public enum Severity
+    {
+        Debug,
+        Verbose,
+        Info,
+        Notice,
+        Warning,
+        Error,
+        Critical,
+        Fatal
+    }
+
+    public enum SystemState
+    {
+        Normal,
+        Stressed,
+        Overloaded,
+        Emergency
+    }
+
+    public class Dispatcher
+    {
+        public static void OverTemperatureEvent()
+        {
+
+        }
+
+        public static void OverVoltageEvent()
+        {
+
+        }
+
+        public static void NetworkConnectivityLossEvent()
+        {
+
+        }
+
+        public static void FanFailureEvent()
+        {
+
+        }
+
+        public static void DiskFailure()
+        {
+
+        }
+
+        public static void MemoryUsageCriticalEvent()
+        {
+
+        }
+
+        public static void ExcessPowerUsageEvent()
+        {
+
+        }
+
+        public static void Method()
+        {
+
+        }
+
+        public static void Method()
+        {
+
+        }
+
+        [DllImport("ntdll.dll", SetLastError = true)]
+        private static extern int NtSetInformationProcess(IntPtr hProcess, int processInformationClass, ref int processInformation, int processInformationLength);
+
+        static void CriticalShutdown(string[] args)
+        {
+            int isCritical = 1;  // we want this to be a Critical Process
+            int BreakOnTermination = 0x1D;  // value for BreakOnTermination (flag)
+
+            Process.EnterDebugMode();  //acquire Debug Privileges
+
+            // setting the BreakOnTermination = 1 for the current process
+            NtSetInformationProcess(Process.GetCurrentProcess().Handle, BreakOnTermination, ref isCritical, sizeof(int));
+        }
+    }
+}
---
+++ SystemMonitor/System.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemMonitor
+{
+    class System
+    {
+        public CPU[] Processors { get; private set; }
+        public GPU[] VideoCards { get; private set; }
+        public RAM[] PhysicalMemory { get; private set; }
+        public Page[] PageFiles { get; private set; }
+        public VRM[] VrmSystems { get; private set; }
+        public Storage[] StorageDevices { get; private set; }
+        public PSU[] PowerSupplies { get; private set; }
+        public RaidCard[] RaidCards { get; private set; }
+        public NIC[] NetworkCards { get; private set; }
+        public Motherboard Motherboard { get; private set; }
+    }
+
+    enum Unit
+    {
+        Base,
+        Kilo,
+        Mega,
+        Giga,
+        Tera
+    }
+
+    class CPU
+    {
+        public byte CoreCount { get; set; }
+        public byte Temp_Max { get; set; }
+        public float VCore_Max { get; set; }
+        public float VCCIO_Max { get; set; }
+        public float VCCSA_Max { get; set; }
+    }
+
+    class GPU
+    {
+        public byte Temp_Max { get; set; }
+        public float VCore_Max { get; set; }
+        public float VMem_Max { get; set; }
+        public ulong Mem_Cap { get; set; }
+    }
+
+    class RAM
+    {
+        public ulong Mem_Cap { get; set; }
+        public ulong[] MemCapPerDIMM { get; set; }
+        public byte DIMM_Count { get; set; }
+        public float VTT_Max { get; set; }
+    }
+
+    class VRM
+    {
+        public ushort Amps_Max { get; set; }
+        public float Temp_Max { get; set; }
+    }
+
+    class Storage
+    {
+        public ulong ByteCapacity { get; set; }
+        public float Temp_Max { get; set; }
+        public float SMART_Status { get; set; }
+    }
+
+    class PSU
+    {
+        public ushort Watts_Max { get; set; }
+        public float Temp_Max { get; set; }
+    }
+
+    class RaidCard
+    {
+        public byte DriveCount_Max { get; set; }
+        public float Temp_Max { get; set; }
+    }
+
+    class NIC
+    {
+        public byte PortCount { get; set; }
+        public Unit PortSpeed_Max { get; set; }
+        public float Temp_Max { get; set; }
+        public bool WOL_Active { get; set; }
+        public bool IsTeamed { get; set; }
+        public bool IsCritical { get; set; }
+    }
+
+    class Motherboard
+    {
+        public float BCLK_Low { get; set; }
+        public float BCLK_High { get; set; }
+        public TempSensor[] TempSensors { get; set; }
+        public Fan[] Fans { get; set; }
+    }
+
+    class Page
+    {
+        public ulong ByteCapacity { get; set; }
+        public byte PercentUsed { get; set; }
+    }
+
+    class Fan
+    {
+        public byte SpeedPercent { get; set; }
+        public bool IsFunctioning { get; set; }
+    }
+
+    class TempSensor
+    {
+        public int Location { get; set; }
+        public byte Temp_Max { get; set; }
+    }
+}
